CaptSilver/wallpaper-engine-kde-plugin

commit 09f7fc29e22a4518e7ba09f9bd45ea5ee6d70bc3

Fix compile error with Plasma 6
Initial attempts to port to KDE Plasma 6 + Qt 6,
but QML things currently breaks Plasma 5.

Uses CMake flag `-D QT_MAJOR_VERSION=6` to control.

Plugin not showing up in setings, but can be loaded if Plasma 6 loads
config of Plasma 5.
Mouse position is also broken.
Some hacks are used, marked with "TODO" in code.

Signed-off-by: fxzjshm <fxzjshm@163.com>

plugin/contents/ui/WindowModel.qml
@@ -1,5 +1,5 @@
 import QtQuick 2.5
-import QtQuick.Controls 1.0
+import QtQuick.Controls 2.0
 import QtQuick.Layouts 1.2
 import QtQuick.Window 2.1
 import org.kde.plasma.core 2.0 as PlasmaCore
@@ -138,13 +138,14 @@ Item {
             return this.data(idx, TaskManager.AbstractTasksModel[property]);
         }
     }
-    PlasmaCore.SortFilterModel {
-        filterRole: 'IsWindow'
-        filterRegExp: 'true'
-        sourceModel: tasksModel
-        onDataChanged: updateWindowsinfo()
-        onCountChanged: updateWindowsinfo()
-    }
+    // TODO: PlasmaCore.SortFilterModel
+    // PlasmaCore.SortFilterModel {
+    //     filterRole: 'IsWindow'
+    //     filterRegExp: 'true'
+    //     sourceModel: tasksModel
+    //     onDataChanged: updateWindowsinfo()
+    //     onCountChanged: updateWindowsinfo()
+    // }
 
     Timer{
         id: triggerTimer

CMakeLists.txt
@@ -28,10 +28,17 @@ endif()
 find_package(ECM REQUIRED NO_MODULE)
 set(CMAKE_MODULE_PATH ${ECM_MODULE_PATH} ${CMAKE_MODULE_PATH})
 include(KDEInstallDirs)
+if(QT_MAJOR_VERSION EQUAL 6)
+find_package(KF6 ${KF5_MIN_VERSION} REQUIRED COMPONENTS
+    Package
+    # Plasma
+)
+else()
 find_package(KF5 ${KF5_MIN_VERSION} REQUIRED COMPONENTS
     Package
     Plasma
 )
+endif()
 
 if(NOT USE_PLASMAPKG)
     plasma_install_package(plugin com.github.casout.wallpaperEngineKde wallpapers wallpaper)

plugin/contents/ui/PowerSource.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.1
 import QtQuick.Layouts 1.3
 import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.plasma5support as Plasma5Support
 
 Item {
 	id: root
@@ -12,7 +13,7 @@ Item {
 
 	// https://github.com/KDE/plasma-workspace/blob/master/dataengines/powermanagement/powermanagementengine.h
 	// https://github.com/KDE/plasma-workspace/blob/master/dataengines/powermanagement/powermanagementengine.cpp
-	PlasmaCore.DataSource {
+	Plasma5Support.DataSource {
 		id: pm_source
 		engine: 'powermanagement'
 		connectedSources: ['Battery'] // basicSourceNames == ["Battery", "AC Adapter", "Sleep States", "PowerDevil", "Inhibitions"]

plugin/contents/ui/Pyext.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.0
 import QtWebSockets 1.0
 import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.plasma5support as Plasma5Support
 
 import "js/jsonrpc.mjs" as Jsonrpc
 
@@ -112,7 +113,7 @@ Item {
         }
     }
 
-    PlasmaCore.DataSource {
+    Plasma5Support.DataSource {
         engine: 'executable'
         connectedSources: [source]
         onNewData: {

plugin/contents/ui/WallpaperListModel.qml
@@ -1,5 +1,5 @@
 import QtQuick 2.5
-import QtQuick.Controls 1.0
+import QtQuick.Controls 2.0
 import QtQuick.Layouts 1.2
 
 import "js/utils.mjs" as Utils

plugin/contents/ui/main.qml
@@ -1,7 +1,9 @@
 import QtQuick 2.12
 import QtQuick.Window 2.2
 import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.plasmoid
 
+WallpaperItem {
 Rectangle {
     id: background
     anchors.fill: parent
@@ -158,7 +160,7 @@ Rectangle {
     WindowModel {
         id: windowModel
         screenGeometry: wallpaper.parent.screenGeometry
-        activity: wallpaper.parent.activity
+        activity: ""// TODO: wallpaper.parent.activity: error: assign null to string
         filterByScreen: wallpaper.configuration.PauseFilterByScreen
         modePlay: wallpaper.configuration.PauseMode
         resumeTime: wallpaper.configuration.ResumeTime
@@ -345,4 +347,5 @@ Rectangle {
         lauchPauseTimer.start();
         randomizeTimer.start();
     }
+}
 }

src/CMakeLists.txt
@@ -1,4 +1,5 @@
-find_package(Qt5 COMPONENTS Quick Qml REQUIRED)
+message(STATUS "QT_MAJOR_VERSION = ${QT_MAJOR_VERSION}")
+find_package(Qt${QT_MAJOR_VERSION} COMPONENTS Quick Qml REQUIRED)
 
 add_subdirectory(backend_mpv)
 
@@ -19,8 +20,8 @@ add_library(${PROJECT_NAME}
 
 target_link_libraries(${PROJECT_NAME} 
 	PRIVATE
-	Qt5::Quick
-	Qt5::Qml 
+	Qt::Quick
+	Qt::Qml 
 	mpvbackend
 	wescene-renderer-qml
 )

src/backend_mpv/CMakeLists.txt
@@ -1,7 +1,7 @@
 project(mpvbackend)
 
-find_package(Qt5 COMPONENTS Gui Quick X11Extras REQUIRED)
-include_directories(${Qt5Gui_PRIVATE_INCLUDE_DIRS})
+find_package(Qt${QT_MAJOR_VERSION} COMPONENTS Gui Quick REQUIRED)
+include_directories(${Qt${QT_MAJOR_VERSION}Gui_PRIVATE_INCLUDE_DIRS})
 
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(MPV REQUIRED mpv)
@@ -17,9 +17,9 @@ add_library(${PROJECT_NAME}
 )
 target_link_libraries(${PROJECT_NAME} 
 	PRIVATE 
-    Qt5::Quick
-	Qt5::Gui
-	Qt5::X11Extras
+    Qt::Quick
+	Qt::Gui
+	# Qt::X11Extras
 	${MPV_LIBRARIES}
 )
 target_include_directories(${PROJECT_NAME} PUBLIC .) #${INCLUDE_DIRECTORIES})

commit 4c471d36f026ad260ef699fe248922844f38629c

Recover SortFilterModel in WindowModel.qml
Fixes: 09f7fc2 ("Fix compile error with Plasma 6")
Signed-off-by: fxzjshm <fxzjshm@163.com>

plugin/contents/ui/WindowModel.qml
@@ -3,6 +3,7 @@ import QtQuick.Controls 2.0
 import QtQuick.Layouts 1.2
 import QtQuick.Window 2.1
 import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.plasma5support as Plasma5Support
 
 import org.kde.taskmanager 0.1 as TaskManager
 
@@ -138,14 +139,14 @@ Item {
             return this.data(idx, TaskManager.AbstractTasksModel[property]);
         }
     }
-    // TODO: PlasmaCore.SortFilterModel
-    // PlasmaCore.SortFilterModel {
-    //     filterRole: 'IsWindow'
-    //     filterRegExp: 'true'
-    //     sourceModel: tasksModel
-    //     onDataChanged: updateWindowsinfo()
-    //     onCountChanged: updateWindowsinfo()
-    // }
+
+    Plasma5Support.SortFilterModel {
+        filterRole: 'IsWindow'
+        filterRegExp: 'true'
+        sourceModel: tasksModel
+        onDataChanged: updateWindowsinfo()
+        onCountChanged: updateWindowsinfo()
+    }
 
     Timer{
         id: triggerTimer